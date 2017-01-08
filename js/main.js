e5a3bde22cd7694fff6901d3f0eacfe043bed1d8
Jason Natividad <jainatividad@yahoo.com>
2017-01-07 23:07:12 -0800

    Add link to resume and add drop down menu functionality for hamburger menu.  No menu items listed yet

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 
+	// Functionality for hamburger menu
 
 var hamburger = document.getElementById('hamburger');
 
-hamburger.addEventListener("click", function(){alert('test'); });
+// hamburger.addEventListener("click", function(){alert('test'); });
 
+hamburger.addEventListener("click", openMenu);
+
+
+	// variables for menu use
+	var header = document.getElementById('main');
+	var menuState = "";
+
+function openMenu(){
+	
+
+	// header.style.height = "500px"
+
+	// alert('test');
+	// console.log();
+
+	
+
+	if ((menuState == "") || (menuState == "closed")){
+		menuState = "open";
+		header.style.height = "500px";
+
+	}
+	else {
+		// alert('test');
+		menuState = "closed";
+		header.style.height = "100px";
+	}
+
+
+}
 
 // var testBtn = document.getElementById('test-btn');
 
